78943cb0e77d21cacb935805de7721f6e2547cbb
Joshua Brokaw <joshuabrokaw@gmail.com>
2017-12-22 17:36:18 -0600

    all the things.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,17 @@
 import { combineReducers } from 'redux';
+import { createStore } from 'redux'
 
 import BooksReducer from './reducer_books';
+import ActiveBookReducer from './reducer_active_book';
 
 const rootReducer = combineReducers({
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBookReducer
 });
 
 export default rootReducer;
+
+
+const store = createStore(
+  rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
